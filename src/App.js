fd615dea4d0d32e55707c1ce7fa8c9d8e57b634b
asif <mhamudasif2@gmail.com>
2022-07-25 17:04:01 +0600

    factoring

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css'
 import Navbar from './Navbar'
-import Home from './Home'
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom'
 import ThemeContextProvider from './Theme/ThemeContext'
 import Recipe from './Recipe'
